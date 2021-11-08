10d0702094bc63e65bc1833a75466ff3a74a4db2
Margit Tennosaar <margit.tennosaar@bc.fi>
2021-11-08 13:07:51 +0200

    added form and view and connected with app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import Form from "./components/Form";
+import View from "./components/View";
 
 class App extends Component {
+  state = {
+    firstname: "",
+    lastname: "",
+    phonenumber: "",
+    message: "",
+    role: "",
+  };
+
+  inputHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
   render() {
-    return <div>App will be here</div>;
+    return (
+      <div>
+        <Form change={this.inputHandler} />
+        <View
+          first={this.state.firstname}
+          last={this.state.lastname}
+          phone={this.state.phone}
+          role={this.state.role}
+          message={this.state.message}
+        />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Form = (props) => {
+  return (
+    <form>
+      <div>
+        <label htmlFor="firstname">First name</label>
+        <input
+          type="text"
+          name="firstname"
+          id="firstname"
+          required
+          onChange={props.change}
+        />
+      </div>
+      <div>
+        <label htmlFor="lastname">Last name</label>
+        <input
+          type="text"
+          name="lastname"
+          id="lastname"
+          required
+          onChange={props.change}
+        />
+      </div>
+      <div>
+        <label htmlFor="phone">Phonenumber</label>
+        <input
+          type="phone"
+          name="phone"
+          id="phone"
+          required
+          onChange={props.change}
+        />
+      </div>
+      <div>
+        <label htmlFor="role">Role</label>
+        <select name="role" id="role" onChange={props.change}>
+          <option value="teacher">Teacher</option>
+          <option value="student">Student</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          name="message"
+          id="message"
+          required
+          onChange={props.change}
+        ></textarea>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
